6c8223abd4bb586e5b83535aab89de2eba5f3e0d
Mathias Kühn <mathias.kuehn@jeppesen.com>
2018-11-27 14:00:36 +0100

    Fixed few issues, just minor

--- parser.js
+++ parser.js
@@ -1,7 +1,16 @@
+'use strict';
+
+/**
+ * Defines a custom parser for the SerialPort library that processes
+ * the datagrams received from the Elero Stick and collects all data
+ * for a single datagram before sending the data back to the caller
+ */
+
 const Transform = require('stream').Transform
 
 /**
- * Emit data every number of bytes
+ * Emit data for a single Elero datagram. The length varies depending on the content header
+ * 
  * @extends Transform
  * @param {Object} options parser options object
  * @summary A transform stream that emits data as a buffer after a specific number of bytes are received. Runs in O(n) time.
@@ -12,49 +21,60 @@ class EleroParser extends Transform {
   constructor(options = {}) {
     super(options)
 
+    // Current position in the receiving buffer
     this.position = 0
+
+    // This is the maximum length of a datagram
     this.maxLength = 7
+
+    // The expected length for the current datagram
     this.length = 0
 
-    // We expect not more than 5 bytes per datagram
+    // We expect not more than 7 bytes per datagram
     this.buffer = Buffer.alloc(this.maxLength)
   }
 
-  _transform(chunk, encoding, cb) {
+  _transform(chunk, encoding, callback) {
     let cursor = 0
     while (cursor < chunk.length) {
-      this.buffer[this.position] = chunk[cursor]
-      cursor++
-      this.position++
+        this.buffer[this.position] = chunk[cursor]
+        cursor++
 
-      // Check if this is an Elero message, if not, flush the data
-      if (this.position == 1) {
-        if (this.buffer[0] != 0xAA) {
-            this.push(this.buffer)
-            this.buffer = Buffer.alloc(this.length)
-            this.position = 0
+        // Check if this is an Elero message, if not, flush the data
+        if (this.position == 0) {
+            if (this.buffer[0] != 0xAA) {
+                this.push(this.buffer)
+                this.buffer = Buffer.alloc(this.length)
+                this.position = 0
 
-            continue
+                continue
+            }
         }
-      }
 
-      // Determine length of payload and add one header byte and one checksum byte
-      if (this.position == 2) {
-        this.length = 1 + this.buffer[1] + 1;
-      }
+        // When the second byte is received, we can determine the length
+        // of the payload and add one header byte and one checksum byte
+        if (this.position == 1) {
+            this.length = 1 + this.buffer[1] + 1;
+        }
+        
+        // We can proceed
+        this.position++
 
-      if (this.position === this.length) {
-        this.push(this.buffer)
-        this.buffer = Buffer.alloc(this.length)
-        this.position = 0
-      }
+        // Check if we read all data 
+        if (this.position === this.length) {
+            this.push(this.buffer)
+            this.buffer = Buffer.alloc(this.maxLength)
+            this.position = 0
+        }
     }
-    cb()
+
+    callback()
   }
 
   _flush(cb) {
     this.push(this.buffer.slice(0, this.position))
     this.buffer = Buffer.alloc(this.maxLength)
+    this.position = 0
     cb()
   }
 }
